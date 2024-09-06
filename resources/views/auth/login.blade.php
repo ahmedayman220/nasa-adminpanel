9d9b15e5539c089fe2f9ddecc545a4731e18493c
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-06 03:16:20 +0300

    Admin Panel g-recaptcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,75 @@
 @extends('layouts.app')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-6">
-        <div class="card mx-4">
-            <div class="card-body p-4">
-                <div class="text-center m-2">
-                    <img src="{{ asset('images/nasa_logo_dark.png') }}" width="120" alt="">
-                </div>
-                @if(session('message'))
-                    <div class="alert alert-info" role="alert">
-                        {{ session('message') }}
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card mx-4">
+                <div class="card-body p-4 text-center">
+                    <div class="text-center m-2">
+                        <img src="{{ asset('images/nasa_logo_dark.png') }}" width="120" alt="">
                     </div>
-                @endif
+                    @if(session('message'))
+                        <div class="alert alert-info" role="alert">
+                            {{ session('message') }}
+                        </div>
+                    @endif
 
 
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                    <div class="input-group mb-3 mt-3">
-                        <div class="input-group-prepend">
+                        <div class="input-group mb-3 mt-3">
+                            <div class="input-group-prepend">
                             <span class="input-group-text">
                                 <i class="fa fa-user"></i>
                             </span>
-                        </div>
-
-                        <input id="email" name="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}">
-
-                        @if($errors->has('email'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('email') }}
                             </div>
-                        @endif
-                    </div>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                            <input id="email" name="email" type="text"
+                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required
+                                   autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}"
+                                   value="{{ old('email', null) }}">
+
+                            @if($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
                         </div>
 
-                        <input id="password" name="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required placeholder="{{ trans('global.login_password') }}">
-
-                        @if($errors->has('password'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('password') }}
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-lock"></i></span>
                             </div>
-                        @endif
-                    </div>
 
+                            <input id="password" name="password" type="password"
+                                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required
+                                   placeholder="{{ trans('global.login_password') }}">
 
-                    <div class="row  mb-4">
-                        <div class="col-6">
-                            <button type="submit" class="btn btn-primary px-4">
-                                {{ trans('global.login') }}
-                            </button>
+                            @if($errors->has('password'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            @endif
                         </div>
-                    </div>
 
-                    <div class="g-recaptcha" data-sitekey="6LdunDYqAAAAAAcXojGilJ91ifysHZCEF8piOx7A" data-action="LOGIN"></div>
 
-                </form>
+                        <div class="row  mb-4">
+                            <div class="col-6">
+                                <button type="submit" class="btn btn-primary px-4">
+                                    {{ trans('global.login') }}
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="g-recaptcha" data-sitekey="6LdunDYqAAAAAAcXojGilJ91ifysHZCEF8piOx7A"
+                                     data-action="LOGIN"></div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
