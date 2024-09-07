5f2e8d797313018982142f8722f87d362385cb8d
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-07 13:23:49 +0300

    edit options

--- resources/views/admin/participantWorkshopAssignments/index.blade.php
+++ resources/views/admin/participantWorkshopAssignments/index.blade.php
@@ -168,7 +168,7 @@
     ],
     orderCellsTop: true,
     order: [[ 1, 'desc' ]],
-    pageLength: 100,
+    pageLength: 200,
   };
   let table = $('.datatable-ParticipantWorkshopAssignment').DataTable(dtOverrideGlobals);
   $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
