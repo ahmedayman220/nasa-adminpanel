5d4dd50dd86e9b3683ecdbe56de1dd7ba170e727
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-08 18:29:04 +0300

    Bootcamp Attendee

--- database/factories/BootcampParticipantFactory.php
+++ database/factories/BootcampParticipantFactory.php
@@ -17,14 +17,14 @@ class BootcampParticipantFactory extends Factory
         return [
             'uuid' => $this->faker->uuid,
             'name_en' => $this->faker->name,
-            'name_ar' => $this->faker->name, // Assuming this could be Arabic, but using name here
+            'name_ar' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'age' => $this->faker->numberBetween(18, 60),
             'phone_number' => $this->faker->phoneNumber,
             'educational_institute' => $this->faker->company,
             'graduation_year' => $this->faker->year,
             'position' => $this->faker->jobTitle,
-            'national' => $this->faker->unique()->numerify('NAT#######'), // Unique national ID
+            'national' => $this->faker->unique()->numerify('NAT#######'),
             'is_participated' => $this->faker->randomElement(['0', '1']),
             'participated_year' => $this->faker->optional()->year,
             'is_attend_formation_activity' => $this->faker->randomElement(['0', '1']),
@@ -32,6 +32,15 @@ class BootcampParticipantFactory extends Factory
             'is_have_team' => $this->faker->randomElement(['0', '1', '2']),
             'comment' => $this->faker->optional()->text,
             'year' => $this->faker->year,
+
+            // Default Foreign Keys
+            'educational_level_id' => 2,  // Fixed value
+            'field_of_study_id' => 1,  // Fixed value
+            'first_priority_id' => 1,  // Fixed value
+            'second_priority_id' => 1,  // Fixed value
+            'third_priority_id' => 1,  // Fixed value
+            'created_by_id' => 1,  // Fixed value
+
             'created_at' => now(),
             'updated_at' => now(),
         ];
--- database/seeders/BootcampParticipantSeeder.php
+++ database/seeders/BootcampParticipantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\BootcampParticipant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,5 @@ class BootcampParticipantSeeder extends Seeder
     {
         // Generate 100 random bootcamp participants
         BootcampParticipant::factory()->count(100)->create();
-
     }
 }
