6d6ff13a47fa621f9f70c8a3a1283f506883cbe8
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-08 18:23:22 +0300

    Bootcamp Attendee

---
+++ database/factories/BootcampParticipantFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\BootcampParticipant;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BootcampParticipant>
+ */
+class BootcampParticipantFactory extends Factory
+{
+    protected $model = BootcampParticipant::class;
+
+    public function definition()
+    {
+        return [
+            'uuid' => $this->faker->uuid,
+            'name_en' => $this->faker->name,
+            'name_ar' => $this->faker->name, // Assuming this could be Arabic, but using name here
+            'email' => $this->faker->unique()->safeEmail,
+            'age' => $this->faker->numberBetween(18, 60),
+            'phone_number' => $this->faker->phoneNumber,
+            'educational_institute' => $this->faker->company,
+            'graduation_year' => $this->faker->year,
+            'position' => $this->faker->jobTitle,
+            'national' => $this->faker->unique()->numerify('NAT#######'), // Unique national ID
+            'is_participated' => $this->faker->randomElement(['0', '1']),
+            'participated_year' => $this->faker->optional()->year,
+            'is_attend_formation_activity' => $this->faker->randomElement(['0', '1']),
+            'why_this_workshop' => $this->faker->sentence,
+            'is_have_team' => $this->faker->randomElement(['0', '1', '2']),
+            'comment' => $this->faker->optional()->text,
+            'year' => $this->faker->year,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
