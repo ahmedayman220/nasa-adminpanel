3e6e89bce267ea25e6a03de4c860b1d98fa73192
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-09 22:04:08 +0300

    Generate uuid

---
+++ app/Console/Commands/GenerateUuidsForBootcampParticipants.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\BootcampParticipant;
+use Illuminate\Support\Str;
+
+class GenerateUuidsForBootcampParticipants extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:bootcamp-participant-uuids';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate UUIDs for bootcamp participants without UUIDs';
+
+    /**
+     * Execute the console command.
+     */
+
+        public function handle()
+    {
+        // Get all bootcamp participants without UUIDs
+        $participants = BootcampParticipant::whereNull('uuid')->get();
+
+        foreach ($participants as $participant) {
+            $participant->uuid = Str::uuid();
+            $participant->save();
+        }
+
+        $this->info('UUIDs generated for ' . $participants->count() . ' bootcamp participants.');
+    }
+}
