989080a5b5c6701fe00dfb8b50beeace97cd0a19
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-15 21:29:49 +0300

    Bootcamp Confirmations api

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,10 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin',
 ], function () {
 //    Route::post('logout','UserAuthController@logout');
     // Education Level
-//    Route::get('education-levels', 'EducationLevelApiController@index');
+    Route::get('education-levels', 'EducationLevelApiController@index');
 
     // Mention Your Field
-//    Route::get('mention-your-fields', 'MentionYourFieldApiController@index');
+    Route::get('mention-your-fields', 'MentionYourFieldApiController@index');
 
     // Bootcamp Form Descriptions
 //    Route::post('bootcamp-form-descriptions/media', 'BootcampFormDescriptionsApiController@storeMedia')->name('bootcamp-form-descriptions.storeMedia');
@@ -17,14 +17,15 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin',
 
     // Study Level
 //    Route::get('study-levels', 'StudyLevelApiController@index');
+    Route::get('slots', 'StudyLevelApiController@index');
 
     // Workshops
-//    Route::post('workshops/media', 'WorkshopsApiController@storeMedia')->name('workshops.storeMedia');
-//    Route::get('workshops', 'WorkshopsApiController@index');
+    Route::post('workshops/media', 'WorkshopsApiController@storeMedia')->name('workshops.storeMedia');
+    Route::get('workshops', 'WorkshopsApiController@index');
 
     // Bootcamp Participants
-//    Route::post('bootcamp-participants/media', 'BootcampParticipantsApiController@storeMedia')->name('bootcamp-participants.storeMedia');
-//    Route::post('bootcamp-participants', 'BootcampParticipantsApiController@store');
+    Route::post('bootcamp-participants/media', 'BootcampParticipantsApiController@storeMedia')->name('bootcamp-participants.storeMedia');
+    Route::post('bootcamp-participants', 'BootcampParticipantsApiController@store');
 
     // Assets History
 //    Route::apiResource('assets-histories', 'AssetsHistoryApiController', ['except' => ['store', 'show', 'update', 'destroy']]);
@@ -38,5 +39,5 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin',
     Route::get('chatbot-traning-datas', 'ChatbotTraningDataApiController@index');
 
     // Bootcamp Confirmation
-    Route::apiResource('bootcamp-confirmations', 'BootcampConfirmationApiController');
+    Route::post('bootcamp-confirmations', 'BootcampConfirmationApiController@store');
 });
