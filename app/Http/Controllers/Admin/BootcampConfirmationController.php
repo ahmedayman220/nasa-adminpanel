b206c9338f9af63262a36cb40c3cf83cee3ed0ee
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-18 22:09:54 +0300

    Send Emails From Bootcamp Confirmations

--- app/Http/Controllers/Admin/BootcampConfirmationController.php
+++ app/Http/Controllers/Admin/BootcampConfirmationController.php
@@ -52,7 +52,9 @@ class BootcampConfirmationController extends Controller
                 return $row->id ? $row->id : '';
             });
             $table->editColumn('first_priority', function ($row) {
-                return $row->national->first_priority->title ? $row->national->first_priority->title : '';
+                return $row->national && $row->national->first_priority
+                    ? $row->national->first_priority->title
+                    : '';
             });
             $table->editColumn('name', function ($row) {
                 return $row->name ? $row->name : '';
