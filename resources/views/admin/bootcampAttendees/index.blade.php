b7b21421c37ed64a1293ba47a0b5bda4c0c586dd
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-21 08:27:45 +0300

    pageLength: 10,

--- resources/views/admin/bootcampAttendees/index.blade.php
+++ resources/views/admin/bootcampAttendees/index.blade.php
@@ -262,7 +262,7 @@
                 ],
                 orderCellsTop: true,
                 order: [[1, 'desc']],
-                pageLength: 100,
+                pageLength: 10,
             };
             let table = $('.datatable-BootcampAttendee').DataTable(dtOverrideGlobals);
             $('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
