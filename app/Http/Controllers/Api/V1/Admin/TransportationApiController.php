c2b7e42c802f97cfbab561db86ae4e5b993dae80
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-23 16:01:35 +0300

    Apis

--- app/Http/Controllers/Api/V1/Admin/TransportationApiController.php
+++ app/Http/Controllers/Api/V1/Admin/TransportationApiController.php
@@ -18,7 +18,7 @@ class TransportationApiController extends Controller
 
     public function index()
     {
-        abort_if(Gate::denies('transportation_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+//        abort_if(Gate::denies('transportation_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return new TransportationResource(Transportation::with(['created_by'])->get());
     }
