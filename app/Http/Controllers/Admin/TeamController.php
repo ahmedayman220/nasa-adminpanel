d7ff7e21a2a5aac9eb4f42cf7574b9229ee62115
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-27 07:27:34 +0300

    userUserChallenges get

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -32,7 +32,7 @@ class TeamController extends Controller
         if ($request->ajax()) {
             $user = auth()->user(); // Or use User::find($userId);
 
-            return response()->json($user->userUserChallenges());
+            return response()->json($user->userUserChallenges()->get());
 
 //            $query = Team::with(['team_leader', 'challenge', 'actual_solution', 'mentorship_needed', 'participation_method'])
 //                ->select(sprintf('%s.*', (new Team)->table));
