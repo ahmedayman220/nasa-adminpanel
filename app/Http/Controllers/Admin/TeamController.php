5e7e556ed3b55d42fae5ea408568021bc2d5a27a
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-27 08:08:01 +0300

    userUserChallenges get

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -34,7 +34,7 @@ class TeamController extends Controller
 
             $userChallenge = $user->userUserChallenges()->first();
 
-            return response()->json($userChallenge->userChallenge());
+            return response()->json($userChallenge->userChallenge()->first());
 
 //            $query = Team::with(['team_leader', 'challenge', 'actual_solution', 'mentorship_needed', 'participation_method'])
 //                ->select(sprintf('%s.*', (new Team)->table));
