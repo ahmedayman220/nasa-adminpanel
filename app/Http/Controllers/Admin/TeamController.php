d68074b1edda58c95e3aa133f62665e29d194e68
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-27 06:56:27 +0300

    return response()->json($userChallenges);

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -36,7 +36,7 @@ class TeamController extends Controller
             $user = Auth::user();
 
             // Get UserChallenges for the authenticated user
-            $userChallenges = $user->userUserChallenges()->get();
+            $userChallenges = $user->userUserChallenges()->first();
 
             return response()->json($userChallenges);
             // Get Challenges associated with the UserChallenges
