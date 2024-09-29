2cdecae9791d5901b8534eecfefe6c306b4e7610
omarmaged23 <omarmaged292@gmail.com>
2024-09-29 20:55:06 +0300

    Scan QR with permission

--- resources/views/admin/checkpoints/show.blade.php
+++ resources/views/admin/checkpoints/show.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.admin')
-@section('content')
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/scanner.css')}}"/>
+@endsection
 
+@section('content')
+    @if(session()->has('success'))
+        <div class="alert alert-success" role="alert">
+            {{session()->get('success')}}
+        </div>
+    @endif
+    @if(session()->has('failed'))
+        <div class="alert alert-danger" role="alert">
+            {{session()->get('failed')}}
+        </div>
+    @endif
 <div class="card">
     <div class="card-header">
         {{ trans('global.show') }} {{ trans('cruds.checkpoint.title') }}
@@ -12,6 +25,9 @@
                 <a class="btn btn-default" href="{{ route('admin.checkpoints.index') }}">
                     {{ trans('global.back_to_list') }}
                 </a>
+                <button class="btn btn-warning scan-Qrcode" data-toggle="modal">
+                    Scan Qr Code
+                </button>
             </div>
             <table class="table table-bordered table-striped">
                 <tbody>
@@ -84,4 +100,54 @@
     </div>
 </div>
 
+    {{--Qr Scanner --}}
+    <div class="container mx-auto qrcode-container hide-scanner">
+        <p class="close-scanning">x</p>
+        <div id="qr-reader">
+
+        </div>
+        <div id="qr-reader-results"></div>
+    </div>
+    <div class="overlay hide-scanner"></div>
+
+    {{--End Qr Scanner --}}
+@endsection
+@section('scripts')
+    <script src="https://unpkg.com/html5-qrcode"></script>
+    <script>
+        var resultContainer = document.getElementById("qr-reader-results");
+        var lastResult,
+            countResults = 0;
+
+        function onScanSuccess(decodedText, decodedResult) {
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                // Handle on success condition with the decoded message.
+                console.log(`Scan result ${decodedText}`, decodedResult);
+                document.location = "/admin/checkpoints/scan/" + decodedText +"{{'/'.$checkpoint->id . '/' . $checkpoint->name}}";
+            }
+        }
+        var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {
+            fps: 10,
+            qrbox: 250,
+        });
+        html5QrcodeScanner.render(onScanSuccess);
+
+        // Hide and Display the scanner functionality
+        const scanBtn = document.querySelector(".scan-Qrcode");
+        const closeScannerBtn = document.querySelector(".close-scanning");
+        const qrContainer = document.querySelector(".qrcode-container");
+
+        function displayQrScanner() {
+            qrContainer.classList.remove("hide-scanner");
+        }
+        function hideQrScanner() {
+            qrContainer.classList.add("hide-scanner");
+        }
+
+        scanBtn.addEventListener("click", displayQrScanner);
+        closeScannerBtn.addEventListener("click", hideQrScanner);
+
+    </script>
 @endsection
