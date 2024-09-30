e7046d3785c906e4246ccd9dbed1533c15d86603
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-10-01 01:58:50 +0300

    ..

--- app/Mail/OnsiteAcceptedOnsiteMail.php
+++ app/Mail/OnsiteAcceptedOnsiteMail.php
@@ -32,7 +32,7 @@ class OnsiteAcceptedOnsiteMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Onsite Accepted Onsite Mail',
+            subject: 'NASA Space Apps Cairo 2024 Hackathon Invitation',
         );
     }
 
