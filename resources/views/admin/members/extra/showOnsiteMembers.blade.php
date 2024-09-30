9e6479bb2ca6d3cff337f9f74f2b5a6cbac1d0b0
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-09-30 21:13:44 +0300

    ..

--- resources/views/admin/members/extra/showOnsiteMembers.blade.php
+++ resources/views/admin/members/extra/showOnsiteMembers.blade.php
@@ -251,7 +251,7 @@
                 ],
                 orderCellsTop: true,
                 order: [[ 1, 'desc' ]],
-                pageLength: 100,
+                pageLength: 1000,
             };
             let table = $('.datatable-Member').DataTable(dtOverrideGlobals);
             $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
