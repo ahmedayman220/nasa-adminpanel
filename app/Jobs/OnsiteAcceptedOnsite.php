88c6d0f61de843d6b4130862fbddf7c1cd9743dd
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-10-01 01:11:19 +0300

    ..

--- app/Jobs/OnsiteAcceptedOnsite.php
+++ app/Jobs/OnsiteAcceptedOnsite.php
@@ -1,6 +1,9 @@
+<?php
+
 namespace App\Jobs;
 
 use App\Mail\OnsiteAcceptedOnsiteMail;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -12,45 +15,45 @@ use Illuminate\Support\Facades\Log;
 
 class OnsiteAcceptedOnsite implements ShouldQueue
 {
-use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-private $members;
-private $base_url;
-private $team;
+    private $members;
+    private $base_url;
+    private $team;
 
-public function __construct($members, $base_url, $team)
-{
-$this->members = $members;
-$this->base_url = $base_url;
-$this->team = $team;
-}
+    public function __construct($members, $base_url, $team)
+    {
+        $this->members = $members;
+        $this->base_url = $base_url;
+        $this->team = $team;
+    }
 
-/**
-* Execute the job.
-*/
-public function handle(): void
-{
-foreach ($this->members as $member) {
-$relative_path = 'QR/' . $member->uuid . '_' . $member->national . '.png';
-$qr_path = public_path($relative_path);
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->members as $member) {
+            $relative_path = 'QR/' . $member->uuid . '_' . $member->national . '.png';
+            $qr_path = public_path($relative_path);
 
-try {
-QrCode::format('png')->size(200)->generate($member->uuid, $qr_path);
-$qrGeneratedUrl = $this->base_url . '/' . $relative_path;
-Mail::to($member->email)->send(new OnsiteAcceptedOnsiteMail($this->team, $member, $qrGeneratedUrl));
-} catch (\Exception $e) {
-Log::error("Failed to process member: {$member->email}. Error: " . $e->getMessage());
-throw $e;
-}
-}
-}
+            try {
+                QrCode::format('png')->size(200)->generate($member->uuid, $qr_path);
+                $qrGeneratedUrl = $this->base_url . '/' . $relative_path;
+                Mail::to($member->email)->send(new OnsiteAcceptedOnsiteMail($this->team, $member, $qrGeneratedUrl));
+            } catch (Exception $e) {
+                Log::error("Failed to process member: {$member->email}. Error: " . $e->getMessage());
+                throw $e;
+            }
+        }
+    }
 
-/**
-* Handle a job failure.
-*/
-public function failed(\Exception $exception)
-{
-// Log failure, notify admin, etc.
-Log::error('OnsiteAcceptedOnsite Job failed: ' . $exception->getMessage());
-}
+    /**
+     * Handle a job failure.
+     */
+    public function failed(Exception $exception)
+    {
+        // Log failure, notify admin, etc.
+        Log::error('OnsiteAcceptedOnsite Job failed: ' . $exception->getMessage());
+    }
 }
