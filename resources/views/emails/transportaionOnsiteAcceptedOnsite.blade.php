d542f2c26d996136837b63f0a592c1d05600e479
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-10-03 21:07:10 +0300

    ..

--- resources/views/emails/transportaionOnsiteAcceptedOnsite.blade.php
+++ resources/views/emails/transportaionOnsiteAcceptedOnsite.blade.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html>
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to NASA Space Apps Cairo 2024!</title>
+    <title>Welcome to NASA Space Apps Cairo 2024 Hackathon!</title>
     <style>
         @font-face {
             font-family: 'CabinetGrotesk';
             src: url('fonts/CabinetGrotesk-Extrabold.woff2') format('woff2'),
             url('fonts/CabinetGrotesk-Extrabold.woff') format('woff');
-            font-weight: 800;
-            /* Use the appropriate weight for Extrabold */
+            font-weight: 800; /* Use the appropriate weight for Extrabold */
             font-style: normal;
         }
-
         body {
             margin: 0;
             padding: 0;
@@ -23,46 +19,31 @@
             background-color: #f3ffff;
             font-family: Arial, sans-serif;
         }
-
         table {
             border-spacing: 0;
         }
     </style>
 </head>
-
 <body>
 <!-- Outer Table for Background Image -->
-<table width="100%" border="0" cellspacing="0" cellpadding="0"
-       style="background-image: url('{{ url('images/email-template.jpg') }}'); background-size: cover; background-position: center; width: 100%; padding: 40px 0; background-repeat: no-repeat;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="background-image: url('{{ url('images/email-template.jpg') }}'); background-size: cover; background-position: center; width: 100%; padding: 40px 0; background-repeat: no-repeat;">
     <tr>
         <td align="center">
             <!-- Main Container -->
-            <table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#f3ffff"
-                   style="border-radius: 25px; overflow: hidden; margin-top: 75px;">
+            <table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#f3ffff" style="border-radius: 25px; overflow: hidden; margin-top: 75px;">
                 <!-- Header Section -->
                 <tr>
                     <td align="center" style="background-color: #f3ffff; padding: 10px 10px 0 10px;">
-                        <img src="{{ url('images/hackathon-cover.jpg') }}"
-                             alt="NASA Space Apps Cairo 2024" width="100%" height="auto"
-                             style="color: #f3ffff;border-radius: 25px;display: block;box-shadow:0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                        <img src="{{ url('images/hackathon-cover.jpg') }}" alt="NASA Space Apps Cairo 2024" width="100%" height="auto" style="color: #f3ffff;border-radius: 25px;display: block;box-shadow:0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                     </td>
                 </tr>
                 <!-- Greeting Section -->
                 <tr>
-                    <td
-                        style="padding: 20px; font-family: 'CabinetGrotesk', sans-serif; color: #09131c; line-height: 1.5;">
-                        <h1 style="text-align:center; font-size: 24px; margin: 0 0 10px 0; color: #09131c;">Welcome
-                            to NASA Space Apps Cairo 2024!</h1>
-                        <p style="font-size: 14px; color: #09131c;margin: 0 0 20px 0;"><strong>Dear {{ $name  }} : {{ $transportation }}
-                                ,</strong></p>
-                        <p
-                            style="font-size: 14px; color: #09131c; margin: 0 0 20px 0; font-size: 14px; text-align: justify; text-justify: inter-word;">
-                            We are pleased to see you participate in <strong>NASA Space Apps Cairo 2024!</strong>
-                            Congratulations to you and your team <strong>{{ $team->team_name }}</strong> for securing a
-                            spot in the largest Space Apps Challenge local hackathon worldwide.
-                            We are confident your distinctive skills and insights will greatly enhance the hackathon's
-                            success.
-
+                    <td style="padding: 20px; font-family: 'CabinetGrotesk', sans-serif; color: #09131c; line-height: 1.5;">
+                        <h1 style="text-align:center; font-size: 24px; margin: 0 0 10px 0; color: #09131c;">Welcome to NASA Space Apps Cairo 2024 Hackathon!</h1>
+                        <p style="font-size: 14px; color: #09131c;margin: 0 0 20px 0;"><strong>Dear {{$name}},</strong></p>
+                        <p style="font-size: 14px; color: #09131c; margin: 0 0 20px 0; font-size: 14px; text-align: justify; text-justify: inter-word;">
+                            We are pleased to see you participate in <strong>NASA Space Apps Cairo 2024 Hackathon.</strong> This email contains essential information, so please ensure you read it thoroughly.
                         </p>
                     </td>
                 </tr>
@@ -80,7 +61,9 @@
                                     <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;">
                                         <strong>Date:</strong> <em>Friday and Saturday, October 4th and 5th</em>
                                     </p>
-
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;">
+                                        <strong>Time:</strong> <em>8:00 AM</em>
+                                    </p>
                                     <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;">
                                         <strong>Location:</strong> <a
                                             href="https://maps.app.goo.gl/jZHdr4WFSwffjp1V7"
@@ -94,15 +77,8 @@
                 <!-- Important Notes Section -->
                 <tr>
                     <td
-                        style="padding: 20px; font-family: 'CabinetGrotesk', sans-serif; color: #09131c; line-height: 1.5;">
-                        <h2 style="font-size: 22px; margin: 0 0 10px 0; color: #09131c;">Important Notes</h2>
-                        <p
-                            style="font-size: 14px; color: #09131c; margin: 0 0 14px 0; font-size: 14px; text-align: justify; text-justify: inter-word;">
-                            To ensure you are well-prepared, please make sure to read to following notes.
-                            <strong>Every Team Lead will receive another email with the Team Code, Team Leaders' Meeting
-                                Link, and Hackathon Guidelines.</strong>
-                            Transportation details will be sent in another email to all participants.
-                        </p>
+                        style="padding: 20px; font-family: 'CabinetGrotesk', sans-serif; color: #09131c; line-height: 1.5; text-align: center">
+                        <h2 style="font-size: 22px; margin: 0 0 10px 0; color: #09131c;">Team Code: {{ $team->uuid }}</h2>
                     </td>
                 </tr>
 
@@ -124,10 +100,12 @@
                                         <li>Fully charge your laptops and power banks, as power sockets are limited per
                                             team!
                                         </li>
-                                        <li>Install or update any necessary software/tools.</li>
+                                        <li>Only 2 power strips (مشترك) are allowed to be brought/used per team.</li>
+                                        <li>Install/update any necessary software/tool before the hackathon.</li>
                                         <li>Download and backup any files, datasets, models, or resources you'll need.
                                         </li>
                                         <li>Bring your own MiFi, to avoid any internet access/speed issues!</li>
+                                        <li>Bring your prayer rug (مصليه) for the jummah prayer.</li>
                                     </ul>
                                 </td>
                             </tr>
@@ -135,42 +113,29 @@
                     </td>
                 </tr>
 
-                <!-- Ticket Details Section -->
+                <!-- Workshop Section -->
                 <tr>
                     <td style="padding: 0 20px 20px 20px; font-family: Arial, sans-serif; color: #09131c;">
-                        <h2 style="font-size: 22px; margin: 0 0 10px 0; color: #09131c;">Personal Ticket Details
-                        </h2>
-                        <p
-                            style="text-align: justify; text-justify: inter-word; font-size: 14px; color: #09131c; margin: 0 0 5px 0; font-size: 14px;">
-                            You will need your ticket details to register for the event.
-                            Please note that this is a private ticket and cannot be transferred to anyone else.
-                            Make sure to have your <strong> QR Code</strong> and <strong>Ticket ID</strong> ready for
-                            quick and easy entry when approaching the registration desk.
-                        </p>
-                        <p
-                            style="text-align: justify; text-justify: inter-word; font-size: 14px; color: #09131c; margin: 0 0 14px 0; font-size: 14px;">
-                            Please keep your ticket details with you throughout the two days of the event, as our
-                            organizers may need to check them at any time.
-                        </p>
+
                     </td>
                 </tr>
-                <!-- QR Code & Ticket ID -->
+                <!-- Workshop Details Section -->
                 <tr>
                     <td style="padding: 0 20px 20px 20px; font-family: Arial, sans-serif; color: #f3ffff;">
-                        <table style="margin-right: auto; margin-left: auto; min-width: 60%; max-width: 100%;"
-                               border="0" cellspacing="0" cellpadding="0">
+                        <table style="margin-right: auto; margin-left: auto; min-width: 60%;" border="0" cellspacing="0" cellpadding="0">
                             <tr>
-                                <td
-                                    style="background: #202e42; padding: 15px 15px 5px 15px; border-radius: 25px; box-shadow:0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                                    <h2
-                                        style="font-size: 20px; margin: 0 0 15px 0; color: #f3ffff; text-align: center">
-                                        QR Code</h2>
-
-                                    <br/>
-                                    <h2 style="font-size: 20px; color: #f3ffff; text-align: center; margin: 5px;">
-                                        {{ $name }}</h2>
-                                    <h2 style="font-size: 20px; color: #f3ffff; text-align: center; margin: 5px;">
-                                        Ticket ID: {{ $currentMember->uuid }}</h2>
+                                <td style="background: #202e42; padding: 15px; border-radius: 25px; box-shadow:0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                                    <h2 style="font-size: 22px; margin: 0 0 10px 0; color: #f3ffff; text-align: center">Transportation Details</h2>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Transportation Routes:</strong> <em> El-Remaya Square </em></p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Telegram Group:</strong> <a href="https://t.me/+dbnatmARVxM0MDJk" style="color: #f3ffff; text-decoration: underline"><em>https://t.me/+6WpjZGT2RUYzZjI0</em></a></p>
+                                    <p > </p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Transportation Routes:</strong> <em> Adly Mansour Station </em></p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Telegram Group:</strong> <a href="https://t.me/+K_V6XYKX9eVkYTVk" style="color: #f3ffff; text-decoration: underline"><em>https://t.me/+6WpjZGT2RUYzZjI0</em></a></p>
+                                    <p > </p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Transportation Routes:</strong> <em> Abdelmoneam Reyad Bus Stop - Tahrir </em></p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Telegram Group:</strong> <a href="https://t.me/+rd8g81tdoHdkMGE0" style="color: #f3ffff; text-decoration: underline"><em>https://t.me/+6WpjZGT2RUYzZjI0</em></a></p>
+                                    <p > </p>
+                                    <p style="font-size: 14px; margin: 5px 0; color: #f3ffff;"><strong>Note:</strong> <em>Please make sure to join the Telegram group of your submitted route to know the exact pickup location and departure time. Kindly do not share this email/group with anyone, as no one other than those who received this confirmation mail will get onto the bus!</em></p>
                                 </td>
                             </tr>
                         </table>
@@ -180,36 +145,28 @@
                 <!-- Closing Section -->
                 <tr>
                     <td style="padding: 0 20px 0px 20px; font-family: Arial, sans-serif; color: #09131c;">
-                        <p
-                            style="text-align: justify; text-justify: inter-word; font-size: 14px; margin: 0 0 10px 0;">
-                            We are pleased to welcome you to NASA Space Apps Cairo 2024, and we are confident that
-                            our collaboration will significantly influence the future of space exploration. Please
-                            keep an eye out for upcoming emails, as we will provide you with all the necessary
-                            information to fully engage with this remarkable opportunity. Should you have any
-                            inquiries or require support, please feel free to contact us.
+                        <p style="text-align: justify; text-justify: inter-word; font-size: 14px; margin: 0 0 10px 0;">
+                            Our organizers will guide you to the location and help you jump on your bus, so please be sure to follow their instructions and have your National ID ready.
+
+                            Please make sure to follow the instructions provided in this email to ensure the best possible experience at the Hackathon. If you have any questions or require further assistance, feel free to reach out to us.
                         </p>
-                        <p
-                            style="text-align: justify; text-justify: inter-word; font-size: 14px; margin: 0 0 10px 0;">
-                            Once again, congratulations on being part of this remarkable event! Your journey into
-                            the cosmos begins now.
+                        <p style="text-align: justify; text-justify: inter-word; font-size: 14px; margin: 0 0 15px 0;">
+                            We look forward to seeing you at the hackathon and collaborating to make a difference in the world!
                         </p>
                     </td>
                 </tr>
                 <!-- Footer Section -->
                 <tr>
-                    <td
-                        style="background-color: #f3ffff; padding: 20px; font-family: Arial, sans-serif; color: #09131c;">
+                    <td style="background-color: #f3ffff; padding: 20px; font-family: Arial, sans-serif; color: #09131c;">
                         <p style="font-size: 14px; margin: 0 0 10px 0;">
                             Best Regards,
                         </p>
                         <p style="font-size: 14px;">
                             <strong>NASA Space Apps Cairo Team</strong>
                             <br>
-                            Email: <a href="mailto:info@spaceappscairo.com"
-                                      style="color: blue; text-decoration: underline">info@spaceappscairo.com</a>
+                            Email: <a href="mailto:info@spaceappscairo.com" style="color: blue; text-decoration: underline">info@spaceappscairo.com</a>
                             <br>
-                            Website: <a href="www.spaceappscairo.com"
-                                        style="color: blue; text-decoration: underline">www.spaceappscairo.com</a>
+                            Website: <a href="www.spaceappscairo.com" style="color: blue; text-decoration: underline">www.spaceappscairo.com</a>
                         </p>
                     </td>
                 </tr>
@@ -218,5 +175,4 @@
     </tr>
 </table>
 </body>
-
 </html>
