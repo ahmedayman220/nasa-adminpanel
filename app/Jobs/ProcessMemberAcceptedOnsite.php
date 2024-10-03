f564e40007c2dda35d3b21510fc7c08a709f350a
Ahmed Ayman <ahamed.ayman220@gmail.com>
2024-10-03 18:47:28 +0300

    ..

--- app/Jobs/ProcessMemberAcceptedOnsite.php
+++ app/Jobs/ProcessMemberAcceptedOnsite.php
@@ -16,7 +16,7 @@ class ProcessMemberAcceptedOnsite implements ShouldQueue {
 
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     public $tries = 5; // Retry up to 5 times if the job fails
-    public $backoff = 1; // Wait 10 seconds between retries
+    public $backoff = 1; // Wait 1 seconds between retries
 
     private $member;
     private $team;
